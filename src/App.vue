777da5ed4565753cb6877ccea27e576d1f70dd44
Heather Pekrul <heather.pekrul@gmail.com>
2019-10-09 20:09:16 -0500

    Separate nav into component, start design

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Nav />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Nav from '@/components/nav/nav.vue';
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
 
 <style lang="scss">
 @import 'assets/scss';
---
+++ src/components/nav/nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import 'style';
+</style>
