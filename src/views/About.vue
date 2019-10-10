9a2147db8e4b1b05c57e90e0566e36c154aa691a
Heather Pekrul <heather.pekrul@gmail.com>
2019-10-09 23:24:51 -0500

    Move default element styling to a separate file to avoid duplicate selectors added to stylesheet, add about content, various style and content changes in the footer and nav

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container">
+    <h1>About</h1>
+
+    <p>My name is Heather Pekrul and I'm a software engineer. I specialize in web technologies,
+      but I've dabbled in others as well. This site is geared towards those endeavors, but my
+      other interests are linked on <a href="https://heatherpekrul.com">my personal site</a>.</p>
+
+    <p>I mainly write code in JavaScript these days, which I have grown to love. Admittedly, it
+    took me some time to get there, but it was an interesting journey.</p>
+
+    <p>My first forays into tech were in elementary school before my family even had a computer
+      in our home. I was fascinated with computers, and luckily enough my elementary school
+      offered an "open computer lab" session before school where any student could come early
+      before class started to use the computer lab. I have fond memories of playing
+      <a href="https://en.wikipedia.org/wiki/The_Oregon_Trail_(1985_video_game)">Oregon Trail</a>
+      and <a href="https://en.wikipedia.org/wiki/Munchers#Word_Munchers">Word Munchers</a>.</p>
+
+    <p>In the summer between elementary school and junior high, we got a computer with internet
+      access and I spent a lot of time using it to its fullest. I started playing an online game called
+      <a href="https://en.wikipedia.org/wiki/Neopets">Neopets</a>, which allows you to create an online
+      pet, play games, and create an online page for your pets. Obviously, I quickly realized that
+      some people were able to spice up their pages with CSS and I wanted to do the same.
+      Unfortunately, I was also exposed to the marquee tag at this time. At least I got it out of my
+      system at an early age.</p>
+
+    <p>I recall asking my mom for permission to create a
+      <a href="https://en.wikipedia.org/wiki/Yahoo!_GeoCities">GeoCities</a> page at some point
+      shortly after figuring out that I really loved creating web pages, and that I could do so with
+      fewer restrictions outside of Neopets. It snowballed when I eventually purchased my first
+      domain name in 2004 for the cool factor. My birthday money was able to purchase a year of
+      shared hosting, and at that point I was exposed to PHP - which was not, as a teacher mistakenly
+      overheard me discussing in junior high, a narcotic but a whole new world of dynamic web sites.</p>
+
+    <p>In high school I began doing freelance web design and development work in an attempt to save up
+      money to pay for college. Eventually, I made my way to <a href="https://www.k-state.edu">Kansas
+      State University</a> where I majored in <a href="http://www.cs.ksu.edu">Information Systems</a>.
+      At K-State, I learned an immense amount about software design, programming languages, networking,
+      and more. I still remember some very interesting database design examples invoking memes
+      (hi, Dr. Hsu!) and that "caching and locality" was the Hail Mary answer for Dr. Andresen (although
+      writing "Go Huskers" on your papers inevitably got you some leeway).</p>
+
+    <p>All wonderful things must come to an end, and as such I graduated in 2011 from K-State and
+      went on to the real world where I started writing code professionally. Every day is a learning
+      experience just as much as it was in the beginning: but it's something I do because I love it and
+      I'm as passionate about it as the day I learned how to add a glitter trail to the mouse cursor
+      on my Neopets page.</p>
   </div>
 </template>
