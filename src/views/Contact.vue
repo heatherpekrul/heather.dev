baf3e330dd19fbb900a4ed06dcf3350a00ec9c34
Heather Pekrul <heather.pekrul@gmail.com>
2020-02-22 10:18:07 -0600

    reset contact form on successful submit

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,26 +10,26 @@
       Your form could not be submitted. Please try again.
     </div>
 
-    <form @submit.prevent="onSubmit">
+    <form @submit.prevent="onSubmit" ref="contactForm">
       <fieldset>
         <label for="name">
           Name
         </label>
-        <input type="text" maxlength="100" name="name" id="name" v-model="name" required>
+        <input type="text" maxlength="100" name="name" id="name" v-model="form.name" required>
       </fieldset>
 
       <fieldset>
         <label for="email">
           Email Address
         </label>
-        <input type="email" maxlength="250" name="email" id="email" v-model="email" required>
+        <input type="email" maxlength="250" name="email" id="email" v-model="form.email" required>
       </fieldset>
 
       <fieldset>
         <label for="message">
           Message
         </label>
-        <textarea name="message" id="message" v-model="message" required></textarea>
+        <textarea name="message" id="message" v-model="form.message" required></textarea>
       </fieldset>
 
       <vue-recaptcha
@@ -58,15 +58,16 @@ export default {
   },
   data() {
     return {
-      name: '',
-      email: '',
-      message: '',
+      form: {
+        name: '',
+        email: '',
+        message: '',
+      },
       success: null,
     };
   },
   methods: {
     onSubmit() {
-      console.log('whatever');
       this.$refs.recaptcha.execute();
     },
 
@@ -74,12 +75,17 @@ export default {
       const vm = this;
       this.$refs.recaptcha.reset();
 
+      Object.keys(this.form).forEach((k) => {
+        this.form[k] = '';
+      });
+      this.$refs.contactForm.reset();
+
       const formProcessorUrl = 'https://heather.dev/forms/contact.php';
 
       const formData = new FormData();
-      formData.append('name', this.name);
-      formData.append('email', this.email);
-      formData.append('message', this.message);
+      formData.append('name', this.form.name);
+      formData.append('email', this.form.email);
+      formData.append('message', this.form.message);
       formData.append('recaptchaToken', recaptchaToken);
 
       Axios.post(formProcessorUrl, formData)
